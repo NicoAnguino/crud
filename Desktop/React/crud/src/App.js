24868e4a394ccaa3a35a759480444d8677432705
Nico Anguino <nikolas_28_95@hotmail.com>
2021-12-30 09:52:03 -0300

    Eliminar Tarea

--- Desktop/React/crud/src/App.js
+++ Desktop/React/crud/src/App.js
@@ -1,31 +1,36 @@
 
 import React, {useState} from "react";
-import { isEmpty } from "lodash";
+import { isEmpty, size } from "lodash";
 import shortid from "shortid";
 
 //npm i --save lodash
 //npm i --save shortid
 
 function App() {
-  const [task, setTask] = useState('')//ESTADO QUE ALMACENA EL NOMBRE DE LA TAREA NUEVA
+  const [tarea, setTarea] = useState('')//ESTADO QUE ALMACENA EL NOMBRE DE LA TAREA NUEVA
   const [tareas, setTareas] = useState([])
 
 
-const addTask = (e) => {
+const agregarTarea = (e) => {
   e.preventDefault();
-  if(isEmpty(task)){
+  if(isEmpty(tarea)){
     console.log("Tarea vacía.");
     return
   }
 
   const nuevaTarea = {
      id:shortid.generate(),
-     titulo: task
+     titulo: tarea
   }
 
   setTareas([...tareas, nuevaTarea])
   
-  setTask('')
+  setTarea('')
+}
+
+const eliminarTarea = (id) => {
+  const tareasFiltradas = tareas.filter(tarea => tarea.id !== id)
+  setTareas(tareasFiltradas)
 }
 
   return (
@@ -36,29 +41,45 @@ const addTask = (e) => {
       <div className="row">
         <div className="col-8">
             <h4 className="text-center">Lista de Tareas</h4>
-            <ul className="list-group">
+                   
+           {   
 
-              {
-                tareas.map((tarea) => ( 
-                  <li className="list-group-item" key={tarea.id}>
-                  <span className="lead">{tarea.titulo}</span>
-                  <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>    
-                  <button className="btn btn-primary btn-sm float-right">Editar</button>               
-                  </li>
-                ))          
-              }
-          
-            </ul>
+           size(tareas) == 0  ? (
+              <h6 className="text-center">Aún no hay tareas programadas</h6>
+           ) : (
+            <ul className="list-group">
+            {
+              tareas.map((tarea) => ( 
+                <li className="list-group-item" key={tarea.id}>
+                <span className="lead">{tarea.titulo}</span>
+                <button 
+                  className="btn btn-danger btn-sm float-right mx-2"
+                  onClick={() => eliminarTarea(tarea.id)}
+                  >
+                  Eliminar
+                </button>    
+                <button 
+                  className="btn btn-primary btn-sm float-right"
+                  >
+                    Editar
+                </button>               
+                </li>
+              ))          
+            }      
+          </ul>
+           )  
+          }
+
         </div>
         <div className="col-4">
             <h4 className="text-center">Formulario</h4>
-            <form onSubmit={addTask}>
+            <form onSubmit={agregarTarea}>
               <input 
                 type="text" 
                 className="form-control mb-2" 
                 placeholder="Ingrese la tarea..."
-                onChange={(text) => setTask(text.target.value)}
-                value={task}
+                onChange={(text) => setTarea(text.target.value)}
+                value={tarea}
               />
               <button 
                 className="btn btn-dark btn-block"
